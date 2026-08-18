4a7db2cb289dac698a8f1a5a89a77a5918d04bd0
Taanveer22 <taanveer469@gmailcom>
2026-08-18 23:53:43 +0600

    alhamdulliah namecheap live link added

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://flamehash-it-client.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://flamehash-it-client.vercel.app",
+      "https://www.flamehash.com",
+    ],
     credentials: true,
   }),
 );
